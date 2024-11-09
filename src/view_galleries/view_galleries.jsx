a66bc1eef2a9bfd101733ee419b9788748fdd49e
Hunter Winterton <hmwinterton@hotmail.com>
2024-11-09 11:21:34 -0700

    view_galleries react

--- src/view_galleries/view_galleries.jsx
+++ src/view_galleries/view_galleries.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export function View_Galleries() {
 	return (
@@ -10,28 +11,28 @@ export function View_Galleries() {
 				<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
 					<div className="col">
 						<NavLink
-							to="View_Gallery"
+							to="/view"
 							className="d-block"
-							style={{ textDecoration: none }}
+							style={{ textDecoration: "none" }}
 						>
 							<div
 								className="shadow-sm"
-								style="
-									width: 100%;
-									aspect-ratio: 1;
-									overflow: hidden;
-									cursor: pointer;
-								"
+								style={{
+									width: "100%",
+									aspectRatio: "1",
+									overflow: "hidden",
+									cursor: "pointer",
+                                }}
 							>
 								<img
 									src="sample-img.jpg"
 									className="img-fluid"
-									style="
-										width: 100%;
-										height: 100%;
-										object-fit: cover;
-										border-radius: 15px;
-									"
+									style={{
+										width: "100%",
+										height: "100%",
+										objectFit: "cover",
+										borderRadius: "15px",
+                                    }}
 								/>
 							</div>
 						</NavLink>
