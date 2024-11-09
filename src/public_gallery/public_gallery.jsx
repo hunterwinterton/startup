070ec726588e6ba50868bec89b48085f68a1898d
Hunter Winterton <hmwinterton@hotmail.com>
2024-11-09 11:08:44 -0700

    public_gallery react

--- src/public_gallery/public_gallery.jsx
+++ src/public_gallery/public_gallery.jsx
@@ -2,8 +2,24 @@ import React from "react";
 
 export function Public_Gallery() {
 	return (
-		<main className="container-fluid bg-secondary text-center">
-			<div>public gallery displayed here</div>
+		<main className="container my-5">
+            <div className="card p-4 shadow" style={{ maxWidth: "80 %", margin: auto }}>
+				<h2 className="text-center mb-4">Gallery Name</h2>
+				<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
+					<div className="col">
+						<img src="sample-img.jpg" className="img-fluid rounded" />
+					</div>
+					<div className="col">
+						<img src="sample-img.jpg" className="img-fluid rounded" />
+					</div>
+					<div className="col">
+						<img src="sample-img.jpg" className="img-fluid rounded" />
+					</div>
+				</div>
+				<div className="d-flex justify-content-center my-4 button-container">
+					<button className="btn btn-primary me-2">Download Images</button>
+				</div>
+			</div>
 		</main>
 	);
 }
--- src/view_galleries/view_galleries.jsx
+++ src/view_galleries/view_galleries.jsx
@@ -2,8 +2,42 @@ import React from "react";
 
 export function View_Galleries() {
 	return (
-		<main className="container-fluid bg-secondary text-center">
-			<div>view galleries displayed here</div>
+		<main className="container my-md-4 my-lg-5">
+			<div>
+				<div>
+					<h2 className="text-center mb-4">Your Galleries</h2>
+				</div>
+				<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+					<div className="col">
+						<NavLink
+							to="View_Gallery"
+							className="d-block"
+							style={{ textDecoration: none }}
+						>
+							<div
+								className="shadow-sm"
+								style="
+									width: 100%;
+									aspect-ratio: 1;
+									overflow: hidden;
+									cursor: pointer;
+								"
+							>
+								<img
+									src="sample-img.jpg"
+									className="img-fluid"
+									style="
+										width: 100%;
+										height: 100%;
+										object-fit: cover;
+										border-radius: 15px;
+									"
+								/>
+							</div>
+						</NavLink>
+					</div>
+				</div>
+			</div>
 		</main>
 	);
 }
