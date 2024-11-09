f955dd46d1b3b2898c4674d8ba1ef21e4f1f11c3
Hunter Winterton <hmwinterton@hotmail.com>
2024-11-09 11:30:55 -0700

    public_gallery react

--- src/public_gallery/public_gallery.jsx
+++ src/public_gallery/public_gallery.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export function Public_Gallery() {
 	return (
 		<main className="container my-5">
-            <div className="card p-4 shadow" style={{ maxWidth: "80 %", margin: auto }}>
+            <div className="card p-4 shadow" style={{ maxWidth: "80 %", margin: "auto" }}>
 				<h2 className="text-center mb-4">Gallery Name</h2>
 				<div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
 					<div className="col">
