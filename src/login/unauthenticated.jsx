092c6fa1c7aac6ebec0e0f887d45f83f1a8750ac
Hunter Winterton <hmwinterton@hotmail.com>
2024-11-13 15:45:08 -0700

    login service

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -9,80 +9,95 @@ export function Unauthenticated(props) {
 	const [displayError, setDisplayError] = React.useState(null);
 
 	async function loginUser() {
-		localStorage.setItem("userName", userName);
-		props.onLogin(userName);
+		loginOrCreate(`/api/auth/login`);
 	}
 
 	async function createUser() {
-		localStorage.setItem("userName", userName);
-		props.onLogin(userName);
+		loginOrCreate(`/api/auth/create`);
+	}
+
+	async function loginOrCreate(endpoint) {
+		const response = await fetch(endpoint, {
+			method: "post",
+			body: JSON.stringify({ email: userName, password: password }),
+			headers: {
+				"Content-type": "application/json; charset=UTF-8",
+			},
+		});
+		if (response?.status === 200) {
+			localStorage.setItem("userName", userName);
+			props.onLogin(userName);
+		} else {
+			const body = await response.json();
+			setDisplayError(`⚠ Error: ${body.msg}`);
+		}
 	}
 
 	return (
 		<>
-				<div
-					className="card p-4 shadow-lg"
-					style={{ maxWidth: "400px", width: "100%" }}
-				>
-					<h2 className="text-center mb-4">Login to Create a Gallery</h2>
-					<form onSubmit={(e) => e.preventDefault()}>
-						<div className="mb-3">
-							<label htmlFor="email" className="form-label">
-								Email
-							</label>
-							<div className="input-group">
-								<span className="input-group-text">@</span>
-								<input
-									type="email"
-									className="form-control"
-									id="email"
-									value={userName}
-									onChange={(e) => setUserName(e.target.value)}
-									placeholder="your@email.com"
-									required
-								/>
-							</div>
+			<div
+				className="card p-4 shadow-lg"
+				style={{ maxWidth: "400px", width: "100%" }}
+			>
+				<h2 className="text-center mb-4">Login to Create a Gallery</h2>
+				<form onSubmit={(e) => e.preventDefault()}>
+					<div className="mb-3">
+						<label htmlFor="email" className="form-label">
+							Email
+						</label>
+						<div className="input-group">
+							<span className="input-group-text">@</span>
+							<input
+								type="email"
+								className="form-control"
+								id="email"
+								value={userName}
+								onChange={(e) => setUserName(e.target.value)}
+								placeholder="your@email.com"
+								required
+							/>
 						</div>
-						<div className="mb-3">
-							<label htmlFor="password" className="form-label">
-								Password
-							</label>
-							<div className="input-group">
-								<span className="input-group-text">🔒</span>
-								<input
-									type="password"
-									className="form-control"
-									id="password"
-									value={password}
-									onChange={(e) => setPassword(e.target.value)}
-									placeholder="Password"
-									required
-								/>
-							</div>
+					</div>
+					<div className="mb-3">
+						<label htmlFor="password" className="form-label">
+							Password
+						</label>
+						<div className="input-group">
+							<span className="input-group-text">🔒</span>
+							<input
+								type="password"
+								className="form-control"
+								id="password"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								placeholder="Password"
+								required
+							/>
 						</div>
-						<div className="d-grid gap-2">
-							<Button
-								variant="primary"
-								onClick={() => loginUser()}
-								disabled={!userName || !password}
-							>
-								Login
-							</Button>
-							<Button
-								variant="secondary"
-								onClick={() => createUser()}
-								disabled={!userName || !password}
-							>
-								Create
-							</Button>
-						</div>
-					</form>
-				</div>
+					</div>
+					<div className="d-grid gap-2">
+						<Button
+							variant="primary"
+							onClick={() => loginUser()}
+							disabled={!userName || !password}
+						>
+							Login
+						</Button>
+						<Button
+							variant="secondary"
+							onClick={() => createUser()}
+							disabled={!userName || !password}
+						>
+							Create
+						</Button>
+					</div>
+				</form>
+			</div>
 
-				<MessageDialog
-					message={displayError}
-					onHide={() => setDisplayError(null)}
-				/>
+			<MessageDialog
+				message={displayError}
+				onHide={() => setDisplayError(null)}
+			/>
 		</>
 	);
 }
